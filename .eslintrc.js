3cc8de2ef35d6adbe72af830b616976f2b6d6a75
David Wessman <david.wesmn@gmail.com>
2019-09-23 21:45:09 +0200

    Update eslint and vue.config.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
