efee75e604ba682a2265f1fd21937b68c702a110
David Wessman <david.wesmn@gmail.com>
2020-01-08 19:16:41 +0100

    PosterSwitcher: simplify by splitting
    - Picking a new poster or adding a new poster now uses separate views.
    - Always render the selected poster on the top.

--- src/components/PosterSwitcher.vue
+++ src/components/PosterSwitcher.vue
@@ -1,20 +1,65 @@
 <template>
   <Modal name="poster-switcher" :open="open" @close="close">
     <template v-slot:title>
-      <h2 class="text-l font-semibold mr-2">Change poster</h2>
-      <span>Select a poster by clicking it.</span>
+      <div v-if="formMode" class="flex flex-row flex-wrap justify-between border-b border-gray-600">
+        <div class="mr-6 mb-4">
+          <h2 class="text-l font-semibold mr-2">Change poster - add one</h2>
+        </div>
+        <div class="flex flex-row justify-between mb-4">
+          <button
+            class="p-2 ml-1 border rounded border-gray-800 bg-white mr-2 hover:bg-gray-800 hover:border-gray-300 hover:text-white"
+            @click="formMode = false"
+          >
+            Choose poster instead
+          </button>
+          <button
+            type="button"
+            class="p-2 ml-1 rounded border bg-blue-700 border-blue-700 text-white hover:bg-blue-900"
+            @click="close"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+      <div v-else class="flex flex-row flex-wrap justify-between border-b border-gray-600">
+        <div class="mr-6 mb-4">
+          <h2 class="text-l font-semibold mr-2">Change poster - pick one</h2>
+        </div>
+        <div class="flex flex-row justify-between mb-4">
+          <button
+            class="p-2 ml-1 border rounded border-gray-800 bg-white mr-2 hover:bg-gray-800 hover:border-gray-300 hover:text-white"
+            @click="formMode = true"
+          >
+            + Add a poster
+          </button>
+          <button
+            type="button"
+            class="p-2 ml-1 rounded border bg-blue-700 border-blue-700 text-white hover:bg-blue-900"
+            @click="close"
+          >
+            Close
+          </button>
+        </div>
+      </div>
     </template>
     <template v-slot:body>
-      <div class="flex flex-wrap">
+      <div v-if="formMode" class="flex flex-wrap pt-2">
         <PosterForm :poster="newPoster" :on-submit="onSubmitPoster" />
+      </div>
+      <div v-if="!formMode" class="flex flex-wrap">
+        <PosterInformation
+          v-if="selectedPoster != null"
+          :poster="selectedPoster"
+          :selected="true"
+          :frame-rotated="rotated"
+        />
+
         <PosterInformation
           v-for="poster in posters"
           :key="poster.id"
           :poster="poster"
-          :selected="selected(poster)"
           :frame-rotated="rotated"
-          :on-select="onSelectPoster"
-          :close="close"
+          @select="onSelectPoster"
         />
       </div>
     </template>
@@ -53,12 +98,10 @@ export default class PosterSwitcher extends Vue {
   selectedPoster: Poster | null = null;
   newPoster: Poster | null = new Poster(0, "", "", "");
 
+  formMode = false;
+
   get rotated(): boolean {
     return this.frame && this.frame.orientation === Orientation.Landscape;
-  }
-
-  selected(poster: Poster): boolean {
-    return this.selectedPoster !== null && poster.id === this.selectedPoster.id;
   }
 
   @Emit("close")
@@ -71,8 +114,8 @@ export default class PosterSwitcher extends Vue {
     undefined;
   }
 
-  @Watch('frame')
-  onPropertyChanged(value: Frame | null, oldValue: Frame | null) {
+  @Watch("frame")
+  onPropertyChanged(value: Frame | null, oldValue: Frame | null): void {
     if (this.frame === null) {
       this.selectedPoster = null;
     } else {
